81b922b76234e397e32638e463bca137849c0ca2
AayanButt <aayanb134@gmail.com>
2023-09-20 22:30:50 +0100

    Added Local tags

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  owners      = "anyone"
+  environment = "anywhere"
+  name        = "anything"
+  common_tags = {
+    owners      = "anyone-mate"
+    environment = "anywhere-mate"
+  }
+}
